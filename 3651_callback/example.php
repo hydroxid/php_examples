fa4a40059eda7577d5b0f36d569835a455fba1a2
Hanz <m@up4.cz>
2023-03-12 09:09:43 +0100

    Add callback

---
+++ 3651_callback/example.php
@@ -0,0 +1,50 @@
+ <?php
+
+/**
+ * 1
+*/
+
+function addTextWithCallable($string) {
+  return $string . " + text from callable\n";
+}
+
+function printMessage($string, $callable) {
+  echo $callable($string);
+}
+
+printMessage("(1) Hello world", "addTextWithCallable");
+
+
+/**
+ * 2
+*/
+function fun(){
+   echo "(2) Hello World\n";
+}
+echo '(2) is_callable? ' . is_callable("fun") . "\n";
+call_user_func("fun");
+
+
+/**
+ * 3
+*/
+call_user_func(function(){
+  echo "(3) Hello World\n";
+});
+
+
+/**
+ * 4
+*/
+function callableFun()
+{
+   echo '(4) Hello World';
+}
+function runCallable(callable $cf)
+{
+   $cf();
+}
+runCallable('callableFun');
+
+
+?>
